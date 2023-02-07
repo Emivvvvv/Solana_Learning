bd792ad02f17a404baabc9b7344045090dbf6dfa
Emirhan TALA <emirhantala99ff99@gmail.com>
2023-02-07 15:51:08 +0300

    student intro v3

---
+++ solana_student_intro_v3/src/state.rs
@@ -0,0 +1,19 @@
+use borsh::{BorshSerialize, BorshDeserialize};
+use solana_program::{
+    program_pack::{IsInitialized, Sealed},
+};
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct StudentInfo {
+    pub is_initialized: bool,
+    pub name: String,
+    pub msg: String,
+}
+
+impl Sealed for StudentInfo {}
+
+impl IsInitialized for StudentInfo {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
